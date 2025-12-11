5fb27b05660c5ec571782b33ba1aa2672bfa25bd
adiarhmn <adiauliarahmn@gmail.com>
2025-12-11 22:47:52 +0800

    Update Link

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,17 +2,22 @@
 import { defineConfig } from 'astro/config'
 import tailwindcss from '@tailwindcss/vite'
 
+import sitemap from '@astrojs/sitemap';
+
 const BASE_URL = '/';
 // const BASE_URL = '/adiauliarahman.dev/';
 
-// const URL = 'https://adiarhmn.github.io';
-const URL = "https://adiauliarahman.dev/"
+const URL = 'https://adiarhmn.github.io';
+// const URL = "https://adiauliarahman.dev/"
 
 // https://astro.build/config
 export default defineConfig({
   site: URL,
   base: BASE_URL,
+
   vite: {
     plugins: [tailwindcss()],
   },
+
+  integrations: [sitemap()],
 })
