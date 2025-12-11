9a0c42486ab75bf9d571119a748bffd6d49f64aa
adiarhmn <adiauliarahmn@gmail.com>
2025-12-11 20:57:56 +0800

    Fix Base URL

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import tailwindcss from '@tailwindcss/vite'
 // https://astro.build/config
 export default defineConfig({
   site: 'https://adiarhmn.github.io',
-  base: '/adiarhmn',
+  base: '/adirhmn',
   vite: {
     plugins: [tailwindcss()],
   },
