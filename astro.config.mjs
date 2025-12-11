5803c40c7723d541f6365f49025e114025c7c1fe
adiarhmn <adiauliarahmn@gmail.com>
2025-12-11 21:00:33 +0800

    Update Typo

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,13 @@
 import { defineConfig } from 'astro/config'
 import tailwindcss from '@tailwindcss/vite'
 
+// const BASE_URL = '/';
+const BASE_URL = '/adiarhmn';
+
 // https://astro.build/config
 export default defineConfig({
   site: 'https://adiarhmn.github.io',
-  base: '/adirhmn',
+  base: BASE_URL,
   vite: {
     plugins: [tailwindcss()],
   },
