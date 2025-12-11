42b4604184d1433d3325cc4acf4d84e96bd66923
adiarhmn <adiauliarahmn@gmail.com>
2025-12-11 21:32:06 +0800

    Update URL error on adiauliarahman.dev

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,10 +3,10 @@ import { defineConfig } from 'astro/config'
 import tailwindcss from '@tailwindcss/vite'
 
 // const BASE_URL = '/adiarhmn';
-const BASE_URL = '/';
+const BASE_URL = '';
 
 // const URL = 'https://adiarhmn.github.io';
-const URL = "https://adiauliarahman.dev"
+const URL = "https://adiauliarahman.dev/"
 
 // https://astro.build/config
 export default defineConfig({
