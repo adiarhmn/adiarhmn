eac0bf4954150b7e5770765ff0bcf6c08e37b76b
adiarhmn <adiauliarahmn@gmail.com>
2025-12-11 21:26:00 +0800

    Deploy with Custom Domain

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,12 +2,15 @@
 import { defineConfig } from 'astro/config'
 import tailwindcss from '@tailwindcss/vite'
 
-// const BASE_URL = '/';
-const BASE_URL = '/adiarhmn';
+// const BASE_URL = '/adiarhmn';
+const BASE_URL = '/';
+
+// const URL = 'https://adiarhmn.github.io';
+const URL = "https://adiauliarahman.dev"
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://adiarhmn.github.io',
+  site: URL,
   base: BASE_URL,
   vite: {
     plugins: [tailwindcss()],
